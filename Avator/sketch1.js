98cf263048d397984ac4428523c82bb464226e7f
Kakitsan <56282204+Kakitsan@users.noreply.github.com>
2020-03-03 19:19:43 -0800

    button

--- Avator/sketch1.js
+++ Avator/sketch1.js
@@ -1,22 +1,64 @@
-let dogs = [{name:"kate", color:"white"}, {name:"kiki", color:"red"}, {name:"geo", color:"black"}, {name:"Bobo", color:"yellow"}, {name:"Nick", color:"green"}, {name:"boss", color:"brown"}];
+let dogs = [{
+  name: "kate",
+  color: "white"
+}, {
+  name: "kiki",
+  color: "red"
+}, {
+  name: "geo",
+  color: "black"
+}, {
+  name: "Bobo",
+  color: "yellow"
+}, {
+  name: "Nick",
+  color: "green"
+}, {
+  name: "boss",
+  color: "brown"
+}];
 let randomIndex;
 let animating = false;
+let imgloader = [];
+let imgcounter = 0;
+let button;
 // let counter = 0;
 
-function setup(){
-createCanvas(600, 600);
-background(200);
-text("click to randomize", 50, 50);
-// setInterval(changeBackground, 1000);
-// setTimeout(changeBackground, 1000);
+function preload() {
+  for (let i = 0; i <= 8; i++) {
+    imgloader[i] = loadImage(`assets/img_${i}.JPG`);
+  }
 }
 
-function draw(){
-  if(animating == true)
-  {
-      noStroke();
-      fill(random(0, 255), random(0, 255), random(0, 255));
-      ellipse(random(width), random(height), random(50, 200));
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+  text("click to randomize", 50, 50);
+  imageMode(CENTER);
+  frameRate(8);
+  console.log(imgloader);
+  textStyle(BOLD);
+  textAlign(CENTER);
+  button = createButton("Click to randomize");
+  button.mousePressed(buttonPressed);
+  button.class("randomizerButton");
+
+  // setInterval(changeBackground, 1000);
+  // setTimeout(changeBackground, 1000);
+}
+
+function draw() {
+  if (animating == true) {
+    clear();
+    image(imgloader[imgcounter], width / 2, height / 2);
+    if (imgcounter < imgloader.length - 1) {
+      imgcounter++;
+    } else {
+      imgcounter = 0;
+    }
+    // noStroke();
+    // fill(random(0, 255), random(0, 255), random(0, 255));
+    // ellipse(random(width), random(height), random(50, 200));
 
   }
 }
@@ -29,22 +71,23 @@ function draw(){
 //
 // }
 // }
-function randomizer(){
+function randomizer() {
   animating = false;
-  if (dogs[0]){
-    background(random(200,255));
+  if (dogs[0]) {
+    background(random(200, 255));
     randomIndex = int(random(dogs.length));
-      text(`${dogs[randomIndex].name} 's favorite color is ${dogs[randomIndex].color}`, 50, 50);
+    text(`${dogs[randomIndex].name} 's favorite color is ${dogs[randomIndex].color}`, width / 3, height - 380);
+    image(random(imgloader), width / 2, height / 2);
     // text(dogs[randomIndex].name + "'s favorite color is " + dogs[randomIndex].color, 50, 50);
     dogs.splice(randomIndex, 1);
-  }
-    else {
-      background(random(200, 255));
+  } else {
+    background(random(200, 255));
     text("nothing left!", 50, 50);
-    }
+  }
 
 }
-function mousePressed(){
+
+function buttonPressed() {
   animating = true;
   setTimeout(randomizer, 2000);
 
